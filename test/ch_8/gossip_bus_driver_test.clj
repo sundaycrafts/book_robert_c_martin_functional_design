75cf6cb0bae57b8389b9ca1fc37b761de820aa6f
sundaycrafts <sundaycrafts@users.noreply.github.com>
2024-09-06 14:41:40 +0200

    ch-8: assert the secound test

--- test/ch_8/gossip_bus_driver_test.clj
+++ test/ch_8/gossip_bus_driver_test.clj
@@ -6,9 +6,17 @@
     (t/testing "success compile"
         (t/is (= true true)))
     (t/testing "drives one bus at one stop"
-        (let [driver (sut/make-driver "d1" [:s1] #{:r1})
+        (let [driver (sut/make-driver "d1" [:s1] #{:r1}) ; :s1 = station, :r1 = rumor
+            world [driver] ; the world is represented as a vector of drivers
+            new-world (sut/drive world) ; the world after drove drivers
+            ]
+        (t/is (= (count new-world) 1))
+        (t/is (= (-> new-world first :route first) :s1))))
+
+    (t/testing "drives one bus at two stops"
+        (let [driver (sut/make-driver "d1" [:s1 :s2] #{:r1})
             world [driver]
             new-world (sut/drive world)]
         (t/is (= (count new-world) 1))
-        (t/is (= (-> new-world first :route first) :s1))))
+        (t/is (= (-> new-world first :route first) :s2))))
     )
