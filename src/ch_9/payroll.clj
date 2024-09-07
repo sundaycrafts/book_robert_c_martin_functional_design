94ce34ae8aa7ae42d6d1e4dd3aa49798a94b8b29
sundaycrafts <sundaycrafts@users.noreply.github.com>
2024-09-07 15:53:21 +0200

    ch-9: complete

--- src/ch_9/payroll.clj
+++ src/ch_9/payroll.clj
@@ -1,13 +1,37 @@
 (ns ch-9.payroll)
 
+;; interfaces and dispatchers
+(defn get-pay-class [employee]
+    (first (:pay-class employee)))
+
+(defn get-disposition [paycheck-directive]
+    (first (:disposition paycheck-directive)))
+
+(defmulti is-today-payday :schedule)
+(defmulti calc-pay get-pay-class)
+(defmulti dispose get-disposition)
+
+;; supporting functions
+(defn get-employees-to-be-paid-today [today employees]
+    (filter #(is-today-payday % today) employees))
+
+(defn- build-employee [db employee] ; defn- represents a private function for this name space
+    (assoc employee :db db))
+
 (defn get-employees [db]
-    (:employees db))
+    (map (partial build-employee db) (:employees db))) ; partial returns a curried function. (fn [employee]) for here.
 
-(defn get-employees-to-be-paid-today [day employees]
-    employees)
+(defn create-paycheck-directives [ids payments dispositions]
+    (map #(assoc {} :id %1 :amount %2 :disposition %3) ids payments dispositions))
 
+(defn send-paychecks [ids payments dispositions]
+    (for [paycheck-directive
+        (create-paycheck-directives ids payments dispositions)]
+    (dispose paycheck-directive))) ; dispose means action, disposition is a information for dispose.
+
+;; getters
 (defn get-paycheck-amounts [employees]
-    (map (constantly 5000) employees))
+    (map calc-pay employees))
 
 (defn get-ids [employees]
     (map :id employees))
@@ -15,15 +39,7 @@
 (defn get-dispositions [employees]
     (map :disposition employees))
 
-(defn send-paychecks [ids amounts dispositions]
-    (map (fn [id amount disposition]
-        {:type :mail
-         :id id
-         :name "name"
-         :address "home"
-         :amount amount})
-        ids amounts dispositions))
-
+;; facade
 (defn payroll [today db]
     (let [employees (get-employees db)
         employees-to-pay (get-employees-to-be-paid-today today employees)
@@ -32,3 +48,63 @@
         dispositions (get-dispositions employees-to-pay) ; dispositions - way to pay
         ]
     (send-paychecks ids amounts dispositions)))
+
+;; details (outside of architectual border line).
+;; put them on other name space in the real product
+(defn- get-salary [employee]
+    (second (:pay-class employee)))
+
+(defmethod calc-pay :salaried [employee] ; impl which invoked when dispatcher (get-pay-class) returns :slaried
+    (get-salary employee))
+
+(defmethod calc-pay :hourly [employee]
+    (let [db (:db employee)
+        time-cards (:time-cards db)
+        my-time-cards (get time-cards (:id employee)) ; `get` gets value from time-cards by index ((:id employee))
+        [_ hourly-rate] (:pay-class employee) ; [_ hourly-rate] is an expression of distributed binding (destructuring). underscore _ means ignoring
+        hours (map second my-time-cards)
+        total-hours (reduce + hours)]
+    (* total-hours hourly-rate)))
+
+(defmethod calc-pay :commissioned [employee]
+    (let [db (:db employee)
+        sales-receipts (:sales-receipts db)
+        my-sales-receipts (get sales-receipts (:id employee))
+        [_ base-pay commission-rate] (:pay-class employee)
+        sales (map second my-sales-receipts)
+        total-sales (reduce + sales)
+        ]
+    (+ (* total-sales commission-rate) base-pay)
+    ))
+
+(defmethod is-today-payday :weekly [_ today]
+    true)
+
+(defmethod is-today-payday :biweekly [_ today]
+    true)
+
+(defmethod is-today-payday :monthly [_ today]
+    true)
+
+(defmethod dispose :paymaster [paycheck-directive]
+    (println "disposing paycheck...")
+    {:type :deposit
+   :id (:id paycheck-directive)
+   :paymaster (second (:disposition paycheck-directive))
+   :amount (:amount paycheck-directive)})
+
+(defmethod dispose :mail [paycheck-directive]
+    (println "disposing paycheck...")
+    {:type :mail
+   :id (:id paycheck-directive)
+   :name (second (:disposition paycheck-directive))
+   :address (nth (:disposition paycheck-directive) 2)
+   :amount (:amount paycheck-directive)})
+
+(defmethod dispose :deposit [paycheck-directive]
+    (println "disposing paycheck...")
+  {:type :deposit
+   :id (:id paycheck-directive)
+   :routing (second (:disposition paycheck-directive))
+   :account (nth (:disposition paycheck-directive) 2)
+   :amount (:amount paycheck-directive)})
--- test/ch_9/payroll_test.clj
+++ test/ch_9/payroll_test.clj
@@ -4,7 +4,7 @@
         [ch-9.payroll :as sut]))
 
 (t/deftest test-payroll
-    (t/testing "pays one salaried employee at end of month by mail"
+    (t/testing "pays one salaried employee at end of month by mail" ; fixed salary (no flex salary)
         (let [employees [{
                 :id "emp1"
                 :schedule :monthly
@@ -20,4 +20,50 @@
             :amount 5000
             }] (sut/payroll today db))))
         )
+
+    (t/testing "pays one hourly employee on Friday by Direct Deposit"
+        (let [
+            employees [{
+                :id "empid"
+                :schedule :weekly
+                :pay-class [:hourly 15]
+                :disposition [:deposit "routing" "account"]}]
+            time-cards {"empid" [[
+                    "2022-11-12T00:00:00Z"
+                    80/10 ; 80/10 represents an exactly rational number (fractional number), not a *result* of 80/10
+                    ]]}
+            db {:employees employees :time-cards time-cards}
+            friday (instant/read-instant-date "2022-11-18T00:00:00Z")
+        ]
+        (t/is (= [{
+            :type :deposit
+            :id "empid"
+            :routing "routing"
+            :account "account"
+            :amount 120
+            }] (sut/payroll friday db)))
+        ))
+
+    (t/testing "pays one commissioned employee on an even Friday by Paymaster" ; commissioned = fixed salary + flex salary
+        (let [
+            employees [{
+                :id "empid"
+                :schedule :biweekly
+                :pay-class [:commissioned 100 5/100]
+                :disposition [:paymaster "paymaster"]}]
+            time-cards {"empid" [[
+                    "2022-11-12T00:00:00Z"
+                    15000
+                    ]]}
+            sales-receipts {"empid" [["2022-11-12T00:00:00Z" 15000]]}
+            db {:employees employees :sales-receipts sales-receipts}
+            friday (instant/read-instant-date "2022-11-18T00:00:00Z")
+        ]
+        (t/is (= [{
+            :type :deposit
+            :id "empid"
+            :paymaster "paymaster"
+            :amount 850
+            }] (sut/payroll friday db)))
+        ))
     )
