3bab153bec7a6889decbf994353d52d93fb3695d
sundaycrafts <sundaycrafts@users.noreply.github.com>
2024-09-10 16:49:02 +0200

    ch-11: add test as a document

--- src/ch_11/data_flow.clj
+++ src/ch_11/data_flow.clj
@@ -12,10 +12,6 @@
         (assoc state :x (+ x n)
             :cycles (vec (concat cycles [x x])))))
 
-;; execution timing
-;;        addx: .n.n.n.n.n.n.n.n.n.n
-;;        noop: xxxxxxxxxxxxxxxxxxxx
-;; clock cycle: 123456789abcdefghijk
 (defn execute [state lines]
     (if (empty? lines)
         state
@@ -27,14 +23,24 @@
                     "TILT"))]
             (recur state (rest lines)))))
 
+; mock for CRT display behaviour
 (defn render-cycles [cycles]
     (loop [cycles cycles
-        screen ""
-        t 0]
+        screen "" ; screen buffer
+        t 0 ; cycle number
+        ]
     (if (empty? cycles)
+        ;; this partition splits strings to lists of 40 char items each.
+        ;; the second 40 means step, which a distance between the first numbers of each partition.
+        ;; e.g. (partition 3 2 (range 20))
+        ;; ; => ((0 1 2) (2 3 4) (4 5 6) ...)
+        ;;        ↑       ↑       ↑
+        ;; distances for each first numbers are 2 in the example.
         (map #(apply str %) (partition 40 40 "" screen))
         (let [x (first cycles)
             offset (- t x)
+            ; offset is in -1, 0, 1 (-1 <= offset <= 1).
+            ; In other words, offset is in the window rendered pixel
             pixel? (<= -1 offset 1)
             screen (str screen (if pixel? "#" "."))
             t (mod (inc t) 40)]
@@ -42,7 +48,7 @@
 
 (defn execute-file [mode file-name]
     (let [lines (string/split-lines (read-from mode file-name))
-        starting-state {:x 1 :cycles []}
+        starting-state {:x 1 :cycles []} ; state acts as a buffer to render cycles at once
         ending-state (execute starting-state lines)]
     (:cycles ending-state)))
 
@@ -53,9 +59,9 @@
     contents)
 
 (defn print-screen [lines]
-    (doseq [line lines]
-        (println line))
-    true)
+     (doseq [line lines]
+         (println line))
+     true)
 
 ;; (defn -main []
 ;;     (-> "input"
--- test/ch_11/data_flow_test.clj
+++ test/ch_11/data_flow_test.clj
@@ -5,4 +5,10 @@
 (t/deftest data-flow-assertion
     (t/testing "executes file"
         (t/is (= [1 1 1] (sut/execute-file {:mode :stub} "noop\nnoop\nnoop"))))
+
+    (t/testing "addx consumes two cycles and updates register x"
+      (t/is (= {:x 5, :cycles [1 2 2]} (sut/addx 3 {:x 2, :cycles [1]}))))
+
+    (t/testing "noop extract register x's value and append it to the cycles of the buffer state"
+      (t/is (= {:x 1, :cycles [1]} (sut/noop {:x 1 :cycles []}))))
   )
